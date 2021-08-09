44a836ebcb2d5622dc6b3dd7a2f71b16c1a13c12
Yeonwoo-Kim <yeonkim0726@gmail.com>
2021-08-10 00:07:16 +0900

    fix Dockerfile

--- src/main/java/docker/jenkins/controller/Hello.java
+++ src/main/java/docker/jenkins/controller/Hello.java
@@ -2,14 +2,15 @@ package docker.jenkins.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
 
-    @GetMapping("/hello")
-    public String helloController()
-    {
-        return "hello";
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
     }
+
 }
